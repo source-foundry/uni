82184b0961035a2539081194dd9490a0ea4ac049
Chris Simpkins <git.simpkins@gmail.com>
2017-11-09 23:30:34 -0500

    new tests for multi-glyph requests

--- uni_test.go
+++ uni_test.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	//"fmt"
 	"strings"
 	"testing"
 )
 
-func TestUnicodeCodePoints(t *testing.T) {
+func TestUnicodeCodePointsSingle(t *testing.T) {
 	cases := []struct {
 		glyph    string
 		expected string
@@ -26,5 +27,34 @@ func TestUnicodeCodePoints(t *testing.T) {
 		} else if !strings.Contains(response[0], c.expected) {
 			t.Errorf("[FAIL] Glyph '%s' yielded response %s, not expected response %s", c.glyph, response[0], c.expected)
 		}
+	}
+}
+
+func TestUnicodeCodePointsMultiple(t *testing.T) {
+	cases := []struct {
+		glyph    string
+		expected string
+	}{
+		{"j", "U+006A 'j'\n"}, // ASCII
+		{"€", "U+20AC '€'\n"}, // Currency
+		{"β", "U+03B2 'β'\n"}, // Greek and Coptic
+		{"ф", "U+0444 'ф'\n"}, // Cyrillic
+		{"▀", "U+2580 '▀'\n"}, // Block elements
+	}
+
+	var testglyphs []string
+
+	for _, c := range cases {
+		testglyphs = append(testglyphs, c.glyph)
+	}
+
+	response := unicodeSearch(testglyphs)
+
+	if len(response) == 0 {
+		t.Errorf("[FAIL] unicodeSearch did not return a value (len = 0)")
+	} else if !(len(response) == 5) {
+		t.Errorf("[FAIL] Expected five Unicode points in response.  Received %d", len(response))
+	} else if !strings.Contains(response[0], cases[0].expected) {
+		t.Errorf("[FAIL] Expected response %s for first index test but received response %s", cases[0].expected, response[0])
 	}
 }
