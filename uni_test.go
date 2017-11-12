86342cdf8b15030f974dd0b53417b11500c4b6c0
Chris Simpkins <git.simpkins@gmail.com>
2017-11-12 14:52:22 -0500

    new tests of main() function

--- uni_test.go
+++ uni_test.go
@@ -4,6 +4,10 @@ import (
 	"regexp"
 	"strings"
 	"testing"
+	"os"
+	"log"
+	"bytes"
+	"io"
 )
 
 // test version string formatting
@@ -81,4 +85,37 @@ func TestUnicodeCodePointsMultiple(t *testing.T) {
 	} else if !strings.Contains(response[0], cases[0].expected) {
 		t.Errorf("[FAIL] Expected response %s for first index test but received response %s", cases[0].expected, response[0])
 	}
+}
+
+func TestMainFunction(t *testing.T) {
+	os.Args = []string{"uni", "j"}
+	old := os.Stdout // keep backup of stdout
+	r, w, err := os.Pipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout = w
+
+	outC := make(chan string)
+
+	// copy the output in a separate goroutine
+	go func() {
+		var buf bytes.Buffer
+		io.Copy(&buf, r)
+		outC <- buf.String()
+	}()
+
+	main()  // call main function with mock os.Args defined above
+
+	// back to normal state
+	w.Close()
+	os.Stdout = old // restoring the real stdout
+	out := <-outC
+
+	if len(out) == 0 {
+		t.Errorf("[FAIL] Test of main() function did not return standard output response")
+	} else if !strings.HasPrefix(out, "U+006A") {
+		t.Errorf("[FAIL] Expected execution of 'uni f' to return string that begins with 'U+006A', but instead it returned %s", out)
+	}
+
 }
