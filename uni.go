cfd5a458bee09143f27d3b137ae8b5e6c65db81f
Chris Simpkins <git.simpkins@gmail.com>
2017-11-13 23:26:50 -0500

    refactored validation function

--- uni.go
+++ uni.go
@@ -62,19 +62,13 @@ func main() {
 	// if there are no arguments to the executable, check std input stream to see if
 	// this is a line filter request that is piped to executable
 	if len(os.Args) < 2 {
-		stdin := os.Stdin
-		f, err := stdin.Stat()
-		if err != nil { // unable to obtain file data with Stat() method call = fail
-			handleStdInErrors()
-		}
 
-		size := f.Size()
-		if size == 0 { // there does not appear to be any data in the stdin stream = fail
+		if !stdinValidates(os.Stdin) {
 			handleStdInErrors()
 		}
 
 		tmp := new(bytes.Buffer)
-		if _, err := io.Copy(tmp, stdin); err != nil {
+		if _, err := io.Copy(tmp, os.Stdin); err != nil {
 			os.Stderr.WriteString("[Error] Failed to copy std input stream to memory. " + fmt.Sprintf("%v", err))
 			os.Exit(1)
 		}
@@ -97,6 +91,27 @@ func main() {
 
 }
 
+// test os.Stdin for presence of data, if present return true, else return false
+func stdinValidates(stdin *os.File) bool {
+	f, err := stdin.Stat()
+	if err != nil { // unable to obtain file data with Stat() method call = fail
+		return false
+	}
+
+	size := f.Size()
+	if size == 0 { // there does not appear to be any data in the stdin stream = fail
+		return false
+	}
+
+	return true
+}
+
+func handleStdInErrors() {
+	os.Stderr.WriteString("[Error] Please include at least one argument or pipe a string to the executable through the stdin stream.\n")
+	os.Stderr.WriteString(usage)
+	os.Exit(1)
+}
+
 // writes Unicode code point value(s) to standard output stream for glyphs entered as command line arguments
 func unicodeSearch(argv []string) []string {
 	var solist []string
@@ -116,8 +131,4 @@ func unicodeSearch(argv []string) []string {
 	return solist
 }
 
-func handleStdInErrors() {
-	os.Stderr.WriteString("[Error] Please include at least one argument or pipe a string to the executable through the stdin stream.\n")
-	os.Stderr.WriteString(usage)
-	os.Exit(1)
-}
+
